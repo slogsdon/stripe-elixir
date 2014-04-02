2f80895030f2272c7edd36892c7f88a8907cda0e
Shane Logsdon <shane@shanelogsdon.com>
2014-04-02 15:42:01 -0400

    convert binary timestamps

--- lib/stripe/util.ex
+++ lib/stripe/util.ex
@@ -1,5 +1,12 @@
 defmodule Stripe.Util do
-  def datetime_from_timestamp(ts) do
+  def datetime_from_timestamp(ts) when is_binary ts do
+    ts = case Integer.parse ts do
+      :error -> 0
+      {i, _r} -> i
+    end
+    datetime_from_timestamp ts
+  end
+  def datetime_from_timestamp(ts) when is_number ts do
     {{year, month, day}, {hour, minutes, seconds}} = :calendar.gregorian_seconds_to_datetime ts
     {{year + 1970, month, day}, {hour, minutes, seconds}}
   end
