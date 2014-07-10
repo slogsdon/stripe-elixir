68571e2133e5fd8f326beaa172f5c567ed6b5678
Shane Logsdon <shane@shanelogsdon.com>
2014-07-10 14:50:57 -0400

    adding basic customer creation

---
+++ lib/stripe/customer.ex
@@ -0,0 +1,96 @@
+defrecord Stripe.Customer,
+  id: nil,
+  object: "customer",
+  livemode: nil,
+  cards: nil,
+  created: nil,
+  account_balance: nil,
+  currency: nil,
+  default_card: nil,
+  delinquent: nil,
+  description: nil,
+  discount: nil,
+  email: nil,
+  metadata: nil,
+  subscriptions: nil do
+
+  @type id              :: binary
+  @type object          :: binary
+  @type livemode        :: boolean
+  @type cards           :: [] #[Stripe.Card.t]
+  @type created         :: {{1970..10000, 1..12, 1..31}, {0..23, 0..59, 0..59}}
+  @type account_balance :: integer
+  @type currency        :: binary
+  @type default_card    :: binary
+  @type delinquent      :: boolean
+  @type description     :: binary
+  @type discount        :: binary # Stripe.Discount.t
+  @type email           :: binary
+  @type metadata        :: Keyword.t
+  @type subscriptions   :: [Stripe.Subscripton.t]
+
+  record_type id: id,
+              object: object,
+              livemode: livemode,
+              cards: cards,
+              created: created,
+              account_balance: account_balance,
+              currency: currency,
+              default_card: default_card,
+              delinquent: delinquent,
+              description: description,
+              discount: discount,
+              email: email,
+              metadata: metadata,
+              subscriptions: subscriptions
+
+  @moduledoc """
+  ## Attributes
+
+  - `id` - `String`
+  - `object` - `String` - value is "customer"
+  - `livemode` - `Boolean`
+  - `cards` - `List`
+  - `created` - `Tuple`
+  - `account_balance` - `Integer` - Current balance, if any, being stored on the 
+      customer’s account. If negative, the customer has credit to apply to the 
+      next invoice. If positive, the customer has an amount owed that will be 
+      added to the next invoice. The balance does not refer to any unpaid 
+      invoices; it solely takes into account amounts that have yet to be 
+      successfully applied to any invoice. This balance is only taken into 
+      account for recurring charges.
+  - `currency` - `String` - The currency the customer can be charged in for 
+      recurring billing purposes (subscriptions, invoices, invoice items).
+  - `default_card` - `String` - ID of the default credit card attached to the 
+      customer
+  - `delinquent` - `Boolean` - Whether or not the latest charge for the 
+      customer’s latest invoice has failed
+  - `description` - `String`
+  - `discount` - `Stripe.Discount` - Describes the current discount active on 
+      the customer, if there is one.
+  - `email` - `String`
+  - `metadata` - `Keyword` - Keyword A set of key/value pairs that you can 
+      attach to a charge object. It can be useful for storing additional 
+      information about the charge in a structured format.
+  - `subscriptions` - `List` - The customer’s current subscriptions, if any
+  """
+
+  def from_keyword(data) do
+    datetime = Stripe.Util.datetime_from_timestamp data[:date]
+    Stripe.Customer.new(
+      id: data[:id],
+      object: data[:object],
+      cards: data[:cards],
+      created: data[:created],
+      account_balance: data[:account_balance],
+      currency: data[:currency],
+      default_card: data[:default_card],
+      delinquent: data[:delinquent],
+      description: data[:description],
+      discount: data[:discount],
+      email: data[:email],
+      metadata: data[:metadata],
+      subscriptions: data[:subscriptions]
+    )
+  end
+end
---
+++ lib/stripe/customers.ex
@@ -0,0 +1,91 @@
+defmodule Stripe.Customers do
+  @moduledoc """
+  Customer objects allow you to perform recurring charges and track multiple 
+  charges that are associated with the same customer. The API allows you to 
+  create, delete, and update your customers. You can retrieve individual 
+  customers as well as a list of all your customers.
+  """
+
+  @doc """
+  Creates a new customer object.
+
+  ## Arguments
+
+  - `account_balance` - `optional` - An integer amount in cents that is the 
+      starting account balance for your customer. A negative amount represents a 
+      credit that will be used before attempting any charges to the customer’s 
+      card; a positive amount will be added to the next invoice.
+  - `card` - `optional` - The card can either be a token, like the ones returned 
+      by our Stripe.js, or a dictionary containing a user’s credit card details 
+      (with the options shown below). Passing card will create a new card, make 
+      it the new customer default card, and delete the old customer default if 
+      one exists. If you want to add additional cards instead of replacing the 
+      existing default, use the card creation API. Whenever you attach a card to 
+      a customer, Stripe will automatically validate the card.
+      - `number` - required - The card number, as a string without any 
+          separators.
+      - `exp_month` - required - Two digit number representing the card's 
+          expiration month.
+      - `exp_year` - required - Two or four digit number representing the 
+          card's expiration year.
+      - `cvc` - optional, highly recommended - Card security code.
+      - `name` - optional - Cardholder's full name.
+      - `address_line1` - optional
+      - `address_line2` - optional
+      - `address_city` - optional
+      - `address_zip` - optional
+      - `address_state` - optional
+      - `address_country` - optional
+  - `coupon` - `optional` - If you provide a coupon code, the customer will have 
+      a discount applied on all recurring charges. Charges you create through 
+      the API will not have the discount.
+  - `description` - `optional` - An arbitrary string that you can attach to a 
+      customer object. It is displayed alongside the customer in the dashboard. 
+      This will be unset if you POST an empty value.
+  - `email` - `optional` - Customer’s email address. It’s displayed alongside 
+      the customer in your dashboard and can be useful for searching and 
+      tracking. This will be unset if you POST an empty value.
+  - `metadata` - `optional` - A set of key/value pairs that you can attach to a 
+      customer object. It can be useful for storing additional information about 
+      the customer in a structured format. This will be unset if you POST an 
+      empty value.
+  - `plan` - `optional` - The identifier of the plan to subscribe the customer 
+      to. If provided, the returned customer object will have a list of 
+      subscriptions that the customer is currently subscribed to. If you 
+      subscribe a customer to a plan without a free trial, the customer must 
+      have a valid card as well.
+  - `quantity` - `optional` - The quantity you’d like to apply to the 
+      subscription you’re creating (if you pass in a plan). For example, if your 
+      plan is 10 cents/user/month, and your customer has 5 users, you could pass 
+      5 as the quantity to have the customer charged 50 cents (5 x 10 cents) 
+      monthly. Defaults to 1 if not set. Only applies when the plan parameter is 
+      also provided.
+  - `trial_end` - `optional` - Unix timestamp representing the end of the trial 
+      period the customer will get before being charged for the first time. If 
+      set, trial_end will override the default trial period of the plan the 
+      customer is being subscribed to. The special value now can be provided to 
+      end the customer’s trial immediately. Only applies when the plan parameter 
+      is also provided.
+  
+  ## Returns
+
+  Returns a customer object if the call succeeded. The returned object will have 
+  information about subscriptions, discount, and cards, if that information has 
+  been provided. If a non-free plan is specified and a card is not provided 
+  (unless the plan has a trial period), the call will return an error. If a 
+  non-existent plan or a non-existent or expired coupon is provided, the call 
+  will return an error.
+
+  If a card has been attached to the customer, the returned customer object will 
+  have a default_card attribute, which is an ID that can be expanded into the 
+  full card details when retrieving the customer.
+  """
+  def create(params) do
+    obj = Stripe.make_request :post, "customers", params
+    if obj[:object] do
+      Enum.map obj, &Stripe.Customer.from_keyword(&1)
+    else
+      []
+    end
+  end
+end
